6428d0b3b9a87f8c03963d1e485752b9556de9ff
jmorka <jmorka87@gmail.com>
2018-03-26 22:54:45 +0200

    Exercise 3 - My Second Test

---
+++ src/test/java/jmz/selenium/course/SecondTest.java
@@ -0,0 +1,37 @@
+package jmz.selenium.course;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SecondTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void mySecondTest(){
+        driver.get("http://localhost/litecart/admin/");
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("admin");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
